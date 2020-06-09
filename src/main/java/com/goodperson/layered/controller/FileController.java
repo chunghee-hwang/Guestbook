83ae9f30ab524a0345d8e949f521bfb1a2ca7ba2
Chunghee Hwang <hch0821@gmail.com>
2020-06-09 23:33:51 +0900

    업로드, 다운로드 기능을 가진 갤러리 기능 구현

--- src/main/java/com/goodperson/layered/controller/FileController.java
+++ src/main/java/com/goodperson/layered/controller/FileController.java
@@ -8,18 +8,20 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -27,9 +29,19 @@ public class FileController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @GetMapping("/uploadform")
-    public String uploadform() {
-        return "uploadform";
+    @GetMapping("/gallery")
+    public ModelAndView gallery(ModelAndView modelAndView) {
+        final File uploadDirectory = new File("d:/uploads");
+        modelAndView.setViewName("gallery");
+        if (uploadDirectory.exists()) {
+            String[] pictures = uploadDirectory.list();
+            if (pictures != null) {
+                String joinedPictures = Arrays.stream(uploadDirectory.list()).collect(Collectors.joining(","))
+                        .toString();
+                modelAndView.addObject("pictures", joinedPictures);
+            }
+        }
+        return modelAndView;
     }
 
     @GetMapping("/uploadresult")
@@ -40,20 +52,27 @@ public class FileController {
     @PostMapping("/upload")
     public String upload(@RequestParam(required = false) MultipartFile[] files, RedirectAttributes redirectAttributes) {
         final String viewName = "redirect:/uploadresult";
-        if (files == null || files.length == 0) {
-            redirectAttributes.addFlashAttribute("message", "Please upload at least one file.");
+        if (files == null) {
+            redirectAttributes.addFlashAttribute("message", "적어도 한 개의 파일을 업로드해주세요.");
         } else {
             for (MultipartFile file : files) {
                 try {
+                    if (file.getSize() == 0) {
+                        throw new Exception("적어도 한 개의 파일을 업로드해주세요. 파일의 크기가 0일 수도 있습니다.");
+                    }
+
+                    final String contentType = Files.probeContentType(Paths.get(file.getOriginalFilename()));
+                    if (!contentType.startsWith("image/")) {
+                        throw new Exception("이미지만 첨부 가능합니다.");
+                    }
+
                     final File uploadDirectory = new File("d:/uploads");
                     if (!uploadDirectory.exists()) {
                         boolean mkSuccess = uploadDirectory.mkdirs();
                         if (!mkSuccess)
-                            throw new Exception("Cannot create the upload directory.");
+                            throw new Exception("업로드 폴더 생성 실패");
                     }
-                    if (file.getSize() == 0) {
-                        throw new Exception("Please upload at least one file.");
-                    }
+
                     try (FileOutputStream fos = new FileOutputStream(
                             new File(uploadDirectory, file.getOriginalFilename()));
                             InputStream is = file.getInputStream();) {
@@ -67,23 +86,23 @@ public class FileController {
                         }
                     }
                 } catch (Exception e) {
-                    redirectAttributes.addFlashAttribute("message", "Fail to upload the files: " + e.getMessage());
+                    redirectAttributes.addFlashAttribute("message", "사진 업로드 실패: " + e.getMessage());
                     return viewName;
                 }
             }
-            redirectAttributes.addFlashAttribute("message", "Uploaded successfully");
+            redirectAttributes.addFlashAttribute("message", "사진 업로드 성공");
         }
         return viewName;
     }
 
     @GetMapping("/download/{fileName}")
-    public String download(HttpServletResponse response, @PathVariable(required = false) String fileName) {
+    public void download(HttpServletResponse response, @PathVariable(required = false) String fileName) {
         File uploadDirectory = new File("d:/uploads");
         File srcFile = new File(uploadDirectory, fileName);
         try {
             final String contentType = Files.probeContentType(Paths.get(fileName));
             if (fileName == null || contentType == null || !srcFile.exists()) {
-                return "redirect:/uploadform";
+                return;
             }
 
             response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\";");
@@ -102,6 +121,5 @@ public class FileController {
         } catch (IOException e) {
             throw new RuntimeException("File to download file.");
         }
-        return "redirect:/uploadform";
     }
 }
